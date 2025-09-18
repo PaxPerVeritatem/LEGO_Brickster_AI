ede2ce4a7461cbb7461e46ffeedff07a7a05a0c0
dsep <dsep@itu.dk>
2025-09-18 19:41:13 +0200

    Begining inital work on DownloadManager class which should handle Bot downloads  on different webpages

---
+++ DownloadManager.cs
@@ -0,0 +1,30 @@
+namespace LEGO_Brickster_AI;
+
+public class DownloadManager
+{
+
+    private readonly Bot bot;
+    private readonly string url;
+    private readonly IList<string> nameList;
+    Path downloadPath; 
+
+    public DownloadManager(string url)
+    {
+        this.url = url;
+        nameList = [];
+    }
+
+    
+    public static 
+
+
+
+
+    static void Main(string[] args)
+    {
+
+    }
+
+
+
+}
