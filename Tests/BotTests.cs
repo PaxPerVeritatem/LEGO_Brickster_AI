2265048c80b2b7b396051a9c26a94d4c818a82e9
dsep <dsep@itu.dk>
2025-09-24 17:40:09 +0200

    figured out how to a xunit test project with a reference to the dotnet project. Additionally, learned about how to use .sln files to achive this. Ready to begin actual test implementations

--- Tests/BotTests.cs
+++ Tests/BotTests.cs
@@ -1,43 +1,56 @@
-using LEGO_Brickster_AI;
+namespace LEGO_Brickster_AI;
 using Xunit;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Support.UI;
-using System.Security.Cryptography.X509Certificates;
 
-public class testBotTests
+public class BotTests
 {
-    private static readonly string downloadFolderString = @"C:\Users\admin\OneDrive\Skrivebord\LEGO_Brickster_AI\LEGO_Data";
+    private static readonly string downloadFolderString = @"C:\Users\admin\OneDrive\Skrivebord\LEGO_Brickster_AI\LEGO_Brickster_AI\LEGO_Data";
     private static readonly string url = "https://library.ldraw.org/omr/sets";
 
+    private static readonly Bot testBot = new(url, downloadFolderString);
+    private static readonly IWebElement? ancestorElement = null;
+    private static readonly string elementString = "fi-select-input";
+    private static readonly string byMechanism = "CLASSNAME";
 
-    // setup a bot and check that it is not null.
+
+    // setup a bot and check that it is not null. 
+    // Also check that the correct downloadPath is set, if it is defined in bot constructor.
+    // Should be run before every test 
+    
     [Fact]
-    public static void Setup()
+    public void Setup()
     {
-        Bot testBot = new(url, downloadFolderString);
         Assert.NotNull(testBot);
-    }
-    public static int GetClickAmount(Bot testBot,string ByMechanism, int ClickAmount,IWebElement ClickAmountElement, IWebElement? AncestorElement = null)
-    {
-        if (AncestorElement != null)
+        if (testBot.Downloadfolderstring != null && testBot.Options != null)
         {
-            testBot.GoToWebpage(url); 
-            testBot.FindPageElement()
+            string Preferenceddownloadfolderstring = testBot.Options.ToString();
+            Console.WriteLine(Preferenceddownloadfolderstring);
+            //Assert.Equal(testBot.Downloadfolderstring, downloadFolderString);
         }
-        else
-        {
-            testBot.GoToWebpage(url);
-        }
-
-        
-
     }
 
 
-    [Fact]
-    public static bool ExpectedClickAmount(int ClickAmount)
-    {
-        int clickAmount = getClickAmount(ClickAmount);
-    }
+
+
+    // [Fact]
+    // public static bool ExpectedClickAmount(int ClickAmount)
+    // {
+    //     GetClickAmount(testBot, byMechanism, ClickAmount,elementString, ancestorElement);
+    //     public static void GetClickAmount(Bot testBot, string ByMechanism, int ClickAmount, string ElementString, IWebElement? AncestorElement = null)
+    // {
+    //     if (AncestorElement != null)
+    //     {
+    //         testBot.GoToWebpage(url);
+
+
+    //     }
+    //     else
+    //     {
+    //         testBot.GoToWebpage(url);
+    //         IWebElement? element = testBot.FindPageElement(ElementString, ByMechanism);
+    //         Console.WriteLine(element.Text);
+    //     }
+    // }
+
 }
