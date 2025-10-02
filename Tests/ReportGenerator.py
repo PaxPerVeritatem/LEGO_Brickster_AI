89b9cd281ffd7adf4e54328501686cf9daa3321d
dsep <dsep@itu.dk>
2025-10-02 19:06:40 +0200

    made rudimentary test report generator with 'coverit' package  report generator'. I installed report generator as a global tool, but im gonna switch to be project based to add it as dependency

---
+++ Tests/ReportGenerator.py
@@ -0,0 +1,45 @@
+import subprocess
+import yaml
+from pathlib import Path  
+import shutil
+
+
+def getNewReport(): 
+    """
+    This function runs all tests for the test project with the Xplat Code Coverage dotnet test flag.
+    It will generate a new report in the TestResults folder based on the ReportGenerator_config.yaml file.
+    The report will be generated in the targetdir folder specified in the ReportGenerator_config.yaml file.
+    If a report already exists in the targetdir folder, it will be overwritten.
+    """
+    subprocess.run([
+        "dotnet", "test",
+        "--collect", "Xplat Code Coverage"
+     ])
+
+    with open("ReportGenerator_config.yaml") as f : 
+        data = yaml.safe_load(f)
+        reports= data["reports"] 
+        targetdir = data ["targetdir"]
+        #classfilters = data ["classfilters"]
+
+    subprocess.run([
+        "reportgenerator", 
+        f"-reports:{reports}",
+        f"-targetdir:{targetdir}"
+        #f"-classfilters:{classfilters}"
+     ])
+    print("New html test report made\n")
+
+
+
+if __name__ == "__main__": 
+    test_result_folder = Path("TestResults")
+    if(test_result_folder.exists()): 
+        print("Old report found. Deleteing old report\n")
+        shutil.rmtree(test_result_folder)
+        getNewReport()
+    else: 
+        getNewReport()
+    
+
+
